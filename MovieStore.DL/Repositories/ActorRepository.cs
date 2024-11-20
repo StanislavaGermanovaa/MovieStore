f41532d8db9496121595fdefae4a88d909bfd200
Stanislava Germanova <114156419+StanislavaGermanovaa@users.noreply.github.com>
2024-11-20 17:44:53 +0200

    init Actor

---
+++ MovieStore.DL/Repositories/ActorRepository.cs
@@ -0,0 +1,19 @@
+﻿using MovieStore.DL.DB;
+using MovieStore.DL.Interfaces;
+using MovieStore.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieStore.DL.Repositories
+{
+    public class ActorRepository : IActorRepository
+    {
+        public List<Actor> GetActors()
+        {
+            return StaticData.Actors;
+        }
+    }
+}
